6d7107ad85db6f99e5bd8cf3ec9f92f705588ac3
Cris Forno <cforno1@binghamton.edu>
2018-03-08 17:52:18 -0500

    something is wrong with the 1 bit predictor

--- proj1/predictor.cpp
+++ proj1/predictor.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 void always(string n_t, ifstream* in);
 void one_bit(int size, ifstream* in);
+int hexadecimalToDecimal(char hexVal[]);
 
 int main(int argc, char** argv){
     //==========================================================================
@@ -32,7 +33,7 @@ int main(int argc, char** argv){
     input.open(argv[1]);
 
     if(!input || !output){
-        fprintf(stderr, "File does not exist!\n");
+        fprintf(stderr, "File(s) does not exist!\n");
         exit(1);
     }
 
@@ -56,20 +57,51 @@ int main(int argc, char** argv){
 
     one_bit(16, &input);
 
-    //create N size table
-    //mod the PC to get index of table and change the state of the value.
-    // use an array to create a table
+    // input.clear();
+    // input.seekg(0);
+
+    // one_bit(32, &input);
+
+    // input.clear();
+    // input.seekg(0);
+
+    // one_bit(128, &input);
+
+    // input.clear();
+    // input.seekg(0);
+
+    // one_bit(256, &input);
+
+    // input.clear();
+    // input.seekg(0);
+
+    // one_bit(512, &input);
+
+    // input.clear();
+    // input.seekg(0);
+
+    // one_bit(1024, &input);
+
+    // input.clear();
+    // input.seekg(0);
 
 
+    // one_bit(2048, &input);
 
+    // input.clear();
+    // input.seekg(0);
 
+    cout << endl;
+
+    //==========================================================================
+    //Bimodal Predictor with 2-bit saturating counters
 
     return 0;
 }
 
 void one_bit(int size, ifstream* in){
     string line;
-    char table[size];
+    int table[size];
     const char delim[2] = " ";
     char* token;
     int predicted_right = 0;
@@ -77,42 +109,40 @@ void one_bit(int size, ifstream* in){
 
     //initialize table to Taken (1).
     for (int i = 0; i < size; i++){
-        table[i] = '1';
+        table[i] = 1;
     }
 
-    while(getline(*in, line) && counter != 25){
+    while(getline(*in, line)){
         char* new_line = (char*)line.c_str();
         token = strtok(new_line, delim);
-        for(unsigned int i = 2; i < strlen(token); i++){
-            token[i-2] = token[i];
-        }
-        int address = atoi(token);
+        int address = hexadecimalToDecimal(token);
         int index = address % size;
-        char state = table[index];
+        int state = table[index];
 
         token = strtok(NULL, delim);
 
         if(strcmp(token, "NT") == 0){
-            if(state == '0'){
+            if(state == 0){
                 predicted_right++;
             } else {
-                table[index] = '0';
+                table[index] = 0;
             }
         } else if(strcmp(token, "T") == 0){
-            if(state == '1'){
+            if(state == 1){
                 predicted_right++;
             } else {
-                table[index] = '1';
+                table[index] = 1;
             }
         }
         counter++;
     }
-    cout << "TABLE:" << endl;
-    for(int i = 0; i < size; i++){
-        cout << table[i] << " || ";
-    }
-    cout << endl;
-    cout << predicted_right << "," << counter << ";" <<endl;
+    // cout << endl;
+    // cout << "TABLE:" << endl;
+    // for(int i = 0; i < size; i++){
+    //     cout << "| " << i << "="<< table[i] << " |";
+    // }
+    // cout << endl;
+    cout << predicted_right << "," << counter << "; ";
 }
 
 void always(string n_t, ifstream* in){
@@ -120,7 +150,7 @@ void always(string n_t, ifstream* in){
     char* new_n_t = (char*) n_t.c_str();
     const char delim[2] = " ";
     char* token;
-    int yerp = 0;
+    int predicted_right = 0;
     int counter = 0;
     while(std::getline(*in,line)){
         char* new_line = (char*)line.c_str();
@@ -128,9 +158,44 @@ void always(string n_t, ifstream* in){
         token = strtok(NULL, delim);
         counter++;
         if(strcmp(token, new_n_t) == 0){
-            yerp++;
+            predicted_right++;
         }
     }
     //TODO: make it out put to file
-    cout << yerp << "," << counter << ";" <<endl;
+    cout << predicted_right << "," << counter << ";" <<endl;
+}
+
+
+//retrieved function from:
+// https://www.geeksforgeeks.org/program-hexadecimal-decimal/
+int hexadecimalToDecimal(char hexVal[]) {
+    for(unsigned int i = 2; i < strlen(hexVal); i++){
+        hexVal[i-2] = hexVal[i];
+    }
+    hexVal[strlen(hexVal) - 2] = '\0';
+    int len = strlen(hexVal);
+    // Initializing base value to 1, i.e 16^0
+    int base = 1;
+    int dec_val = 0;
+
+    // Extracting characters as digits from last character
+    for (int i=len-1; i>=0; i--) {
+            // if character lies in '0'-'9', converting
+            // it to integral 0-9 by subtracting 48 from
+            // ASCII value.
+            if (hexVal[i]>='0' && hexVal[i]<='9') {
+                    dec_val += (hexVal[i] - 48)*base;
+                    // incrementing base by power
+                    base = base * 16;
+                }
+            // if character lies in 'A'-'F' , converting
+            // it to integral 10 - 15 by subtracting 55
+            // from ASCII value
+            else if (hexVal[i]>='a' && hexVal[i]<='f') {
+                    dec_val += (hexVal[i] - 87)*base;
+                    // incrementing base by power
+                    base = base*16;
+                }
+        }
+    return dec_val;
 }
