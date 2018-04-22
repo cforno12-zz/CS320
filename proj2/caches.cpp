d092c647aa6c9c9983d938c032b8203b91c2057c
Cris Forno <cforno1@binghamton.edu>
2018-04-22 15:42:15 -0400

    finished direct mapping

--- proj2/caches.cpp
+++ proj2/caches.cpp
@@ -1,1 +1,76 @@
-#include "caches.h"
+#include "caches.hpp"
+
+void direct_mapped(std::string instruct, unsigned int  address, int cache_size, unsigned int cache[], int* counter){
+    //cache size /32
+    int total_cache_size = cache_size * 1024;
+    int cache_line_size = 32;
+    int byte_offset = 5; //2^5 == 32
+
+    unsigned int num_slots = (total_cache_size)/(cache_line_size);
+    unsigned int index = (address >> byte_offset) % (num_slots); //shifting by 5 since the cache line is 32 bytes
+    unsigned int tag = address >> (unsigned int)(log2(num_slots)+byte_offset);
+
+    if(cache[index] == tag){
+        (*counter)++;
+    } else {
+        cache[index] = tag;
+    }
+}
+
+void set_associative(std::string instruct, unsigned int address, int* counter, unsigned int** cache, int set, deque<unsigned int> lru_queue){
+
+    int byte_offset = 5;
+    int cache_line_size = 32;
+    int cache_size = 16 * 1024;
+
+    int num_slots = ((cache_size)/(cache_line_size))/set;
+    unsigned int index = (address >> byte_offset)%(num_slots);
+    unsigned int tag = address >> (unsigned int) (log2(num_slots)+byte_offset);
+
+}
+    //this queue holds the tags
+    // std::deque<unsigned int> lru_queue;
+
+//     //access
+
+//     //check to make sure it isn't in the queue already
+//     bool update = false;
+//     for(int x : lru_queue){
+//         if (lru_queue.at(x) == tag){
+//             update = true;
+//             lru_queue.erase(x);
+//             lru_queue.push_front(tag);
+//             break;
+//         }
+//     }
+//     //replace if queue is full
+//     int replace_this_element_in_cache = -1;
+//     if(lru_queue.size() == cache_size && !update){
+//         replace_this_element_in_cache = lru_queue.at(cache_size-1);
+//         lru_queue.pop_back();
+//     } else if(!update){
+//         lru_queue.push_front(tag);
+//     }
+
+// }
+
+// void lru_helper(std::deque<unsigned int> lru_queue){
+//     //check to make sure it isn't in the queue already
+//     bool update = false;
+//     for(int x : lru_queue){
+//         if (lru_queue.at(x) == tag){
+//             update = true;
+//             lru_queue.erase(x);
+//             lru_queue.push_front(tag);
+//             break;
+//         }
+//     }
+//     //replace if queue is full
+//     int replace_this_element_in_cache = 0;
+//     if(lru_queue.size() == cache_size && !update){
+//         replace_this_element_in_cache = lru_queue.at(cache_size-1);
+//         lru_queue.pop_back();
+//     } else if(!update){
+//         lru_queue.push_front(tag);
+//     }
+// }
--- proj2/driver.cpp
+++ proj2/driver.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <sstream>
+#include "caches.hpp"
 
 using namespace std;
 
@@ -32,7 +33,6 @@ int main(int argc, char** argv){
 
 
     //==========================================================================
-
     //parsing variables
     string line;
     const char delim[2] = " ";
@@ -40,7 +40,66 @@ int main(int argc, char** argv){
 
     int access_counter = 0;
 
-    while(getline(input, line) && access_counter != 25){
+    //==========================================================================
+    //Direct mapping variables
+    int size_of_1 = 0;
+    int size_of_4 = 0;
+    int size_of_16 = 0;
+    int size_of_32 = 0;
+
+    unsigned int direct_map_cache_1[1024]; //2^10 is 1K
+    unsigned int direct_map_cache_4[4096]; //2^12 is 4K
+    unsigned int direct_map_cache_16[16384]; //2^14 is 16K
+    unsigned int direct_map_cache_32[32768]; //2^15 is 32K
+
+    //==========================================================================
+    //Set associative variables
+
+    int set_counter_2 = 0;
+    int set_counter_4 = 0;
+    int set_counter_8 = 0;
+    int set_counter_16 = 0;
+
+    int cache_size_16K = 16 * 1024;
+    int cache_line_size = 32;
+
+    int rows_of_set_2 = (cache_size_16K/cache_line_size)/2;
+    int rows_of_set_4 = (cache_size_16K/cache_line_size)/4;
+    int rows_of_set_8 = (cache_size_16K/cache_line_size)/8;
+    int rows_of_set_16 = (cache_size_16K/cache_line_size)/16;
+
+    unsigned int** set_cache_2 = new unsigned int*[rows_of_set_2];
+    for(int i = 0; i < rows_of_set_2; i++){
+        set_cache_2[i] = new unsigned int[2];
+    }
+
+    unsigned int** set_cache_4 = new unsigned int*[rows_of_set_4];
+    for(int i = 0; i < rows_of_set_4; i++){
+        set_cache_4[i] = new unsigned int[4];
+    }
+
+    unsigned int** set_cache_8 = new unsigned int*[rows_of_set_8];
+    for(int i = 0; i < rows_of_set_8; i++){
+        set_cache_8[i] = new unsigned int[8];
+    }
+    unsigned int** set_cache_16 = new unsigned int*[rows_of_set_16];
+    for(int i = 0; i < rows_of_set_16; i++){
+        set_cache_16[i] = new unsigned int[16];
+    }
+
+    deque<unsigned int> lru_set_queue_2;
+    deque<unsigned int> lru_set_queue_4;
+    deque<unsigned int> lru_set_queue_8;
+    deque<unsigned int> lru_set_queue_16;
+    //==========================================================================
+    //Fully Associative variables
+
+    //int fully_counter_
+
+
+    //==========================================================================
+    //begin parsing file
+    while(getline(input, line)){
         char* new_line = (char*) line.c_str();
         buffer = strtok(new_line, delim);
         string instruction(buffer);
@@ -54,10 +113,28 @@ int main(int argc, char** argv){
         //INSTRCTION is instruction variable
         //ADDRESS is address variable
 
-        
+        //DIRECT MAPPING
+        direct_mapped(instruction, address, 1, direct_map_cache_1, &size_of_1);
+        direct_mapped(instruction, address, 4, direct_map_cache_4, &size_of_4);
+        direct_mapped(instruction, address, 16, direct_map_cache_16, &size_of_16);
+        direct_mapped(instruction, address, 32, direct_map_cache_32, &size_of_32);
+        //SET ASSOCIATIVE
+        set_associative(instruction, address, &set_counter_2, set_cache_2, 2, lru_set_queue_2);
+        set_associative(instruction, address, &set_counter_4, set_cache_4, 4, lru_set_queue_4);
+        set_associative(instruction, address, &set_counter_8, set_cache_8, 8, lru_set_queue_8);
+        set_associative(instruction, address, &set_counter_16, set_cache_16, 16, lru_set_queue_16);
+        //FULLY ASSOCIATIVE
 
 
 
         access_counter++;
     }
+
+    //==========================================================================
+    //Printing results to output file
+
+    output << size_of_1 << "," << access_counter << "; ";
+    output << size_of_4 << "," << access_counter << "; ";
+    output << size_of_16 << "," << access_counter << "; ";
+    output << size_of_32 << "," << access_counter << ";" << endl;
 }
